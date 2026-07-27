dbd39a166c36289af22c1fc9300d273ca08218d6
dear7575 <41235345+dear7575@users.noreply.github.com>
2026-07-27 22:34:41 +0800

    chore: update local site config

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "非你莫属",
+	title: "北港不夏",
 
 	// 站点副标题
 	subtitle: "喜欢你，没道理！",
@@ -106,9 +106,9 @@ export const siteConfig: SiteConfig = {
 		// 打赏页面开关
 		sponsor: false,
 		// 留言板页面开关，需要配置评论系统
-		guestbook: false,
+		guestbook: true,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐
-		bangumi: false,
+		bangumi: true,
 		// 相册页面开关
 		gallery: true,
 		// 追番页面开关
